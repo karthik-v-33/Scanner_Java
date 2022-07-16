014490abd9f1b73b1ff62ea4163088682a8888fa
Karthik <105358710+karthik-v-33@users.noreply.github.com>
2022-07-16 17:28:31 +0530

    Add files via upload

---
+++ Scanner_Java.java
@@ -0,0 +1,18 @@
+package com.GitHub_Updates;
+import java.util.Scanner;
+public class Scanner_Java {
+	public static void main(String[] args) {	
+		        //a2+b2+2ab
+		        Scanner sc =new Scanner(System.in);
+		        //int a,b,c;
+		        float a,b,c;
+		        System.out.println("Enter 2 Nos : ");
+		        //a=sc.nextInt();
+		       // b=sc.nextInt();
+		        a=sc.nextFloat();
+		        b=sc.nextFloat();
+		        c=(a*a)+(b*b)+(2*a*b);
+		        System.out.println("Result : "+c);
+		 
+		    }
+	}
